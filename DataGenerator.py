a43b2b7d8530a6c7edc908280594cbda3d4d2b09
Gerber Florian <gerbf11@bfh.ch>
2024-12-30 18:07:21 +0100

    Add Df generation from csvs with synthetic data.

--- DataGenerator.py
+++ DataGenerator.py
@@ -1,6 +1,7 @@
 import datetime
 import os
 from io import StringIO
+import logging
 
 import pandas as pd
 from dotenv import load_dotenv
@@ -8,6 +9,7 @@ from openai import OpenAI
 
 from KaggleDatasetProvider import KaggleDatasetProvider
 
+LOGGER = logging.getLogger(__name__)
 
 class DataGenerator:
     def __init__(self):
@@ -16,12 +18,12 @@ class DataGenerator:
 
         self.model = "gpt-4o"
 
-    def generate_data(self, df: pd.DataFrame):
+    def generate_data(self, df_for_sample_data: pd.DataFrame):
         question = f"""
-    Please generate 100 rows of new fraudulent transactions data. The provided CSV includes both fraudulent (Class column is 1) and non-fraudulent (Class column is 0) transactions. Ensure the new data has the same columns as the example CSV below, but with new values for each column. The new data should represent fraudulent transactions, where the Class column is 1. Each row should have exactly 31 columns, with the same data types as the example data.
+    Please generate 100 rows of new fraudulent transactions data. The provided CSV includes both fraudulent (Class column is 1) and non-fraudulent (Class column is 0) transactions. It contains only numerical input variables which are the result of a PCA transformation with the exception of the 'Time' and 'Amount' columns. Ensure the new data has the same columns as the example CSV below, but with new values for each column. The new data should represent fraudulent transactions, where the Class column is 1. Each row should have exactly 31 columns, with the same data types as the example data. 
 
     Example CSV:
-        {df.to_csv(index=False)}
+        {df_for_sample_data.to_csv(index=False)}
 
         Respond only with the data.
         """
@@ -52,11 +54,16 @@ if __name__ == "__main__":
 
     generator = DataGenerator()
 
-    data = generator.generate_data(pd.DataFrame(df_for_generation))
-    data = data.split("\n", 1)[1]
-    # Read the data into a pandas DataFrame
-    df_generated = pd.read_csv(StringIO(data))
-    df_generated = df_generated.dropna()
-    # Save the DataFrame to a CSV file
-    df_generated.to_csv(f"data/synthetic_data_{datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}.csv", index=False)
-    print("created file")
+    for _ in range(10):
+        data = generator.generate_data(pd.DataFrame(df_for_generation))
+        data = data.split("\n", 1)[1]
+        # Read the data into a pandas DataFrame
+        try:
+            df_generated = pd.read_csv(StringIO(data))
+            df_generated = df_generated.dropna()
+            # Save the DataFrame to a CSV file
+            timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+            df_generated.to_csv(f"data/synthetic_data_{timestamp}.csv", index=False)
+            LOGGER.info("created file: synthetic_data_{timestamp}.csv")
+        except Exception as e:
+            LOGGER.error(e)
--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+from remove_test_data_from_train_data import remove_test_data_from_train_data
+from df_generator_from_csvs import generate_df_from_csvs
 from run_classification import prepare_undersampled_split, run_classifiers
 from KaggleDatasetProvider import KaggleDatasetProvider
 
 if __name__ == "__main__":
     df = KaggleDatasetProvider().fetch_data()
+    df_synthetic = generate_df_from_csvs('data')
+    df_synthetic = remove_test_data_from_train_data(df_synthetic, df)
     X_test, X_train, y_test, y_train = prepare_undersampled_split(df)
     run_classifiers(X_test, X_train, y_test, y_train)
