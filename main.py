eca540182553c61ecb0affa3504208d6afbb62b3
Gerber Florian <gerbf11@bfh.ch>
2024-12-30 14:17:21 +0100

    move classification into separate class

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from run_classification import run_classification
+from run_classification import prepare_undersampled_split, run_classifiers
 from KaggleDatasetProvider import KaggleDatasetProvider
 
 if __name__ == "__main__":
     df = KaggleDatasetProvider().fetch_data()
-    run_classification(df)
+    X_test, X_train, y_test, y_train = prepare_undersampled_split(df)
+    run_classifiers(X_test, X_train, y_test, y_train)
